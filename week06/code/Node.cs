4bf533583d7595f14c3452d8ce1b1ae456a6ef58
Liam <liamjnell@gmail.com>
2025-10-06 17:37:50 -0600

    Week06 activities

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,7 +12,8 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
-
+        if (value == Data) return;
+        
         if (value < Data)
         {
             // Insert to the left
@@ -34,12 +35,44 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data) return true;
+
+        if (value < Data)
+        {
+            if (Left == null)
+            {
+                return false;
+            }
+            else
+            {
+                return Left.Contains(value);
+            }
+        }
+
+        else
+        {
+            if (Right == null)
+            {
+                return false;
+            }
+            else
+            {
+                return Right.Contains(value);
+            }
+        }
+
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+
+        int leftHeight = 0;
+        int rightHeight = 0;
+
+        if (Left is not null) leftHeight = Left.GetHeight();
+        if (Right is not null) rightHeight = Right.GetHeight();
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
